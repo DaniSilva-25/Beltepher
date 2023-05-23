26214d951250dd6665c837a782be29a405656bb2
DaniSilva-25 <lbelt2010@gmail.com>
2023-05-23 16:22:25 +0100

    Test file done + new string file

--- Beltepher.cpp
+++ Beltepher.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
+#include <fstream>
 #include "Beltepher.h"
 using namespace std;
 
+void welcome(){
+	cout << ">=>>=>               >=>   >=>\n";
+	cout << ">>   >=>             >=>   >=>                      >=>\n";
+	cout << ">>    >=>   >==>     >=> >=>>==>   >==>    >=> >=>  >=>        >==>    >> >==> \n";
+	cout << ">==>>=>   >>   >=>   >=>   >=>   >>   >=>  >>   >=> >=>>=>   >>   >=>   >=>\n";
+	cout << ">>    >=> >>===>>=>  >=>   >=>   >>===>>=> >>   >=> >=>  >=> >>===>>=>  >=>\n";
+	cout << ">>     >> >>         >=>   >=>   >>        >=> >=>  >>   >=> >>         >=>\n";
+	cout << ">===>>=>   >====>   >==>    >=>   >====>   >=>      >=>  >=>  >====>   >==>\n";
+	cout << "                                           >=>\n";
+	system("pause");
+}
+
 int main() {
 	Beltepher bt;
+	ofstream out;
+	out.open("RecentStrings.txt", ios::out|ios::app);
+	system("title Beltepher - Beltech Corporation");
+	welcome();
 	char msg[99];
 	string rotmsg, input, encrypted, shift;
 	int rots, pos, sz;
 
-	cout << "Enter the string: ";
+	cout << "Enter a word: ";
 	cin >> input;
 	cout << "By how many spaces would you like to rotate? ";
 	cin >> rots;
@@ -20,6 +37,8 @@ int main() {
 	shift = bt.hideShift(rotmsg, rots, pos);
 	cout << "Your Beltepher string is: ";
 	cout << shift;
+	out << input << "\t" << shift << "\n";
+	out.close();
 	
 	system("pause > null");
 	return 0;
--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    char message[99];
-    cin.get(message, 99);
-    message[99] = '\n';
-    cout << message;
-}
